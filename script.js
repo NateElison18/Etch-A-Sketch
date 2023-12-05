40880f27353df39449c902cfae6735579b2364c5
NateElison18 <elisonnathan@gmail.com>
2023-12-05 08:26:11 -0700

    Added rainbow mode feature: randomly generates colors

--- script.js
+++ script.js
@@ -1,34 +1,60 @@
 let pixelCount = 16;
+let color = 'black';
+let mode = 'normal';
 buildBoard(pixelCount);
 setButtonListeners();
 
+function generateRandomColor() {
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+
+    color = 'rgb(' + red + ', ' + green + ', ' + blue + ')';
+}
 
 function setButtonListeners() {
     const clearBtn = document.querySelector('#clearBtn');
     const pixelCountBtn = document.querySelector('#pixelCountBtn');
-
+    const rainbowBtn = document.querySelector('#rainbowBtn');
+    
     clearBtn.addEventListener('click', () => {
         clearBoard();
         buildBoard(pixelCount);
     });
 
     pixelCountBtn.addEventListener('click', () => {
-        let count = prompt("Input number between 1 and 100: ");
-        count = parseInt(count);
-
-        if (!Number.isInteger(count) || count > 100 || count < 1 ) {
-            alert("Please enter a number between 1 and 100");
-        } else {
-            pixelCount = count;
-            clearBoard();
-            buildBoard(pixelCount);
-        }
+        getPixelCountFromUser();
     });
+
+    rainbowBtn.addEventListener('click', () => {
+        toggleRainbowMode();
+    });
+
+}
+
+function toggleRainbowMode() {
+    if(mode === 'rainbow') {
+        mode = 'normal';
+        color = 'black';
+    } else
+        mode = 'rainbow';
+}
+
+function getPixelCountFromUser() {
+    let count = prompt("Input number between 1 and 100: ");
+    count = parseInt(count);
+
+    if (!Number.isInteger(count) || count > 100 || count < 1 ) {
+        alert("Please enter a number between 1 and 100");
+    } else {
+        pixelCount = count;
+        clearBoard();
+        buildBoard(pixelCount);
+    }
 }
 
 function clearBoard() {
     const board = document.querySelector('#board');
-    console.log('inside clear board');
     while(board.firstChild) {
         board.removeChild(board.lastChild);
     }
@@ -39,7 +65,11 @@ function buildPixel() {
     pixel.setAttribute('class', 'pixel');
 
     pixel.addEventListener('mousemove', () => {
-        pixel.setAttribute('style', 'background-color: black');
+        if(mode === 'rainbow') {
+            generateRandomColor();
+        }
+
+        pixel.setAttribute('style', 'background-color: ' + color);
 
     });
 
